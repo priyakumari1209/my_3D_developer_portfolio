f652bf97766526ead37e16306a77d92fe5bc9555
Priya Kumari <mishrapriya15987@gmail.com>
2026-02-08 10:18:38 +0530

    update in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,11 +92,13 @@ const Navbar = () => {
             onClick={() => setToggle(!toggle)}
           />
 
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
+<div
+  className={`${
+    !toggle ? "hidden" : "flex"
+  } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl
+     bg-[#6a1b9a]`}
+>
+
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
